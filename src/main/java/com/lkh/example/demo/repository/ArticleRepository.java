5c5cd9f5bd037eb1d4d7ba9776cb6c1398785a58
Leekunhee830 <gorrka159@naver.com>
2022-03-25 22:53:49 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/lkh/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/lkh/example/demo/repository/ArticleRepository.java
@@ -3,9 +3,13 @@ package com.lkh.example.demo.repository;
 
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.lkh.example.demo.vo.Article;
 
@@ -13,6 +17,7 @@ import com.lkh.example.demo.vo.Article;
 public interface ArticleRepository {
 	
 	//insert into article set regdate = NOW(), updateDate = NOW() , title = ? , body = ?
+	@Insert("INSERT INTO article SET regdate = NOW() , updateDate=NOW() , title=#{title} , body=#{body}")
 	public Article writeArticle(String title,String body);
 	
 	//select * from article where id = ?
@@ -20,11 +25,14 @@ public interface ArticleRepository {
 	public Article getArticle(@Param("id") int id);
 			
 	//delete from article where id = ?
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 	
 	//update article set title = ? , boyd = ? , updateDate = NOW() where id = ?
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE article SET title = #{title} , body = #{body} , updateDate = NOW() WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
 	
 	//select * from article order by id desc
+	@Select("SELECT * FROM article ORDER BY id=#{id} DESC")
 	public List<Article> getArticles();
 }
