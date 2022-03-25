8d81a78a449aa14431dd0f2e9d06a6b800100aa7
Leekunhee830 <gorrka159@naver.com>
2022-03-25 23:50:10 +0900

    SQL을 XML 파일로 옮김

--- src/main/java/com/lkh/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/lkh/example/demo/repository/ArticleRepository.java
@@ -17,25 +17,19 @@ import com.lkh.example.demo.vo.Article;
 public interface ArticleRepository {
 	
 	//insert into article set regdate = NOW(), updateDate = NOW() , title = ? , body = ?
-	@Insert("INSERT INTO article SET regdate = NOW() , updateDate=NOW() , title=#{title} , body=#{body}")
 	public void writeArticle(@Param("title") String title,@Param("body") String body);
 	
 	//select * from article where id = ?
-	@Select("select * from article where id = #{id}")
 	public Article getArticle(@Param("id") int id);
 			
 	//delete from article where id = ?
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 	
 	//update article set title = ? , boyd = ? , updateDate = NOW() where id = ?
-	@Update("UPDATE article SET title = #{title} , body = #{body} , updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
 	
 	//select * from article order by id desc
-	@Select("SELECT * FROM article ORDER BY id=#{id} DESC")
 	public List<Article> getArticles();
 	
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 }
