5d5459a486c11ced197bfc54da575b4528ab60e7
Leekunhee830 <gorrka159@naver.com>
2022-03-26 01:00:07 +0900

    회원가입을 처리하기 위해 UsrMemberController , MemberService 추가

--- src/main/java/com/lkh/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrHomeController.java
@@ -1,26 +1,18 @@
 package com.lkh.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lkh.example.demo.vo.Article;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Controller
 public class UsrHomeController {
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public String getString() {
+	public String doJoin() {
 		return "안녕하세요.";
 	}
 	
---
+++ src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
@@ -0,0 +1,26 @@
+package com.lkh.example.demo.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lkh.example.demo.service.MemberService;
+
+
+@Controller
+public class UsrMemberController {
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public String doJoin(String loginId,String loginPw,String name,String nickname,String cellphoneNo,String email) {
+		memberService.join(loginId,loginPw,name,nickname,cellphoneNo,email);
+		return "안녕하세요.";
+	}
+	
+}
+
