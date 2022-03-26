4c9ee82e9f11744b63343325278bcf9865a36087
Leekunhee830 <gorrka159@naver.com>
2022-03-26 22:57:46 +0900

    Ut.empty 함수 도입

--- src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lkh.example.demo.service.MemberService;
+import com.lkh.example.demo.util.Ut;
 import com.lkh.example.demo.vo.Member;
 
 
@@ -20,22 +21,22 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId,String loginPw,String name,String nickname,String cellphoneNo,String email) {
-		if(loginId==null||loginId.trim().length()==0) {
+		if(Ut.empty(loginId)) {
 			return "loginId를 입력해주세요.";
 		}
-		if(loginPw==null||loginPw.trim().length()==0) {
+		if(Ut.empty(loginPw)) {
 			return "loginPw를 입력해주세요.";
 		}
-		if(name==null||name.trim().length()==0) {
+		if(Ut.empty(name)) {
 			return "name를 입력해주세요.";
 		}
-		if(nickname==null||nickname.trim().length()==0) {
+		if(Ut.empty(nickname)) {
 			return "nickname를 입력해주세요.";
 		}
-		if(cellphoneNo==null||cellphoneNo.trim().length()==0) {
+		if(Ut.empty(cellphoneNo)) {
 			return "cellphoneNo를 입력해주세요.";
 		}
-		if(email==null||email.trim().length()==0) {
+		if(Ut.empty(email)) {
 			return "email를 입력해주세요.";
 		}
 		
