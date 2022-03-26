ff7ece76dbbd1aaebb679ec5f3c4d81ae17f2756
Leekunhee830 <gorrka159@naver.com>
2022-03-26 21:16:11 +0900

    MemberRepository 추가 후 회원가입 처리

--- src/main/java/com/lkh/example/demo/service/MemberService.java
+++ src/main/java/com/lkh/example/demo/service/MemberService.java
@@ -1,14 +1,17 @@
 package com.lkh.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lkh.example.demo.repository.MemberRepository;
 
 @Service
 public class MemberService {
-
-	//private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		//memberRepository.join(loginId,loginPw,name,nickname,cellphoneNo,email);
+		memberRepository.join(loginId,loginPw,name,nickname,cellphoneNo,email);
 	}
 
 }
