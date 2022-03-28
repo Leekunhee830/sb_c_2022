83e0357f99cb444ea989f30b71270f4e76f7e4f3
Leekunhee830 <gorrka159@naver.com>
2022-03-28 22:30:26 +0900

    ArticleController에 ResultData 적용

--- src/main/java/com/lkh/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrArticleController.java
@@ -26,11 +26,9 @@ public class UsrArticleController {
 	@Autowired
 	private ArticleService articleService;
 	
-	
-
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData doAdd(String title,String body) {
+	public ResultData<Article> doAdd(String title,String body) {
 		if(Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
@@ -38,22 +36,23 @@ public class UsrArticleController {
 			return ResultData.from("F-1", "body(을)를 입력해주세요.");
 		}
 		
-		ResultData writeArticleRd=articleService.writeArticle(title,body);
-		int id=(int)writeArticleRd.getData1();
+		ResultData<Integer> writeArticleRd=articleService.writeArticle(title,body);
+		int id=writeArticleRd.getData1();
 		Article article=articleService.getArticle(id);
 		
-		return ResultData.from(writeArticleRd.getResultCode(),writeArticleRd.getMsg(),article);
+		return ResultData.newData(writeArticleRd,article);
 	}
 	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List getArticles() {
-		return articleService.getArticles();
+	public ResultData<List<Article>> getArticles() {
+		List<Article> articles= articleService.getArticles();
+		return ResultData.from("S-1", "게시물 리스트 입니다.",articles);
 	}
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData getArticle(int id) {
+	public ResultData<Article> getArticle(int id) {
 		Article article=articleService.getArticle(id);
 		if(article==null) {
 			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id));
@@ -64,30 +63,29 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Article> doDelete(int id) {
 		Article article=articleService.getArticle(id);
 		
 		if(article==null) {
-			return id+"번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
 		articleService.deleteArticle(id);
-		
-		return id+"번 게시물을 삭제하였습니다.";
+		return ResultData.from("S-1",Ut.f("%d번 게시물을 삭제하였습니다.", id),article);
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id,String title,String body) {
+	public ResultData<Article> doModify(int id,String title,String body) {
 		Article article=articleService.getArticle(id);
 		
 		if(article==null) {
-			return id+"번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
 		articleService.modifyArticle(id,title,body);
 		
-		return id+"번 게시물을 수정하였습니다.";
+		return ResultData.from("S-1",Ut.f("%d번 게시물을 수정하였습니다.", id),article);
 	}
 
 	
--- src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.lkh.example.demo.service.MemberService;
 import com.lkh.example.demo.util.Ut;
 import com.lkh.example.demo.vo.Member;
+import com.lkh.example.demo.vo.ResultData;
 
 
 
@@ -20,38 +21,35 @@ public class UsrMemberController {
 	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId,String loginPw,String name,String nickname,String cellphoneNo,String email) {
+	public ResultData doJoin(String loginId,String loginPw,String name,String nickname,String cellphoneNo,String email) {
 		if(Ut.empty(loginId)) {
-			return "loginId를 입력해주세요.";
+			return ResultData.from("F-1", "loginId를 입력해주세요.");
 		}
 		if(Ut.empty(loginPw)) {
-			return "loginPw를 입력해주세요.";
+			return ResultData.from("F-2", "loginPw를 입력해주세요.");
 		}
 		if(Ut.empty(name)) {
-			return "name를 입력해주세요.";
+			return ResultData.from("F-3", "name를 입력해주세요.");
 		}
 		if(Ut.empty(nickname)) {
-			return "nickname를 입력해주세요.";
+			return ResultData.from("F-4", "nickname를 입력해주세요.");
 		}
 		if(Ut.empty(cellphoneNo)) {
-			return "cellphoneNo를 입력해주세요.";
+			return ResultData.from("F-5", "cellphoneNo를 입력해주세요.");
 		}
 		if(Ut.empty(email)) {
-			return "email를 입력해주세요.";
+			return ResultData.from("F-6", "email를 입력해주세요.");
 		}
 		
-		int id=memberService.join(loginId,loginPw,name,nickname,cellphoneNo,email);
+		ResultData joinRd=memberService.join(loginId,loginPw,name,nickname,cellphoneNo,email);
 		
-		if(id==-1) {
-			return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.",loginId);
-		}
-		if(id==-2) {
-			return Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.",name,email);
+		if(joinRd.isFail()) {
+			return joinRd;
 		}
 		
-		Member member=memberService.getMemberById(id);
+		Member member=memberService.getMemberById((int)joinRd.getData1());
 		
-		return member;
+		return ResultData.newData(joinRd,member);
 	}
 	
 }
