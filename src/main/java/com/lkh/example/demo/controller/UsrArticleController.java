1c4165d2f14886df5d6afa40c80510bdccbe6330
Leekunhee830 <gorrka159@naver.com>
2022-03-28 19:16:07 +0900

    ResultData 클래스 생성 , getArticle에 적용

--- src/main/java/com/lkh/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrArticleController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lkh.example.demo.service.ArticleService;
+import com.lkh.example.demo.util.Ut;
 import com.lkh.example.demo.vo.Article;
+import com.lkh.example.demo.vo.ResultData;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,13 +46,13 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article=articleService.getArticle(id);
 		if(article==null) {
-			return id+"번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
-		return article;
+		return ResultData.from("S-1",Ut.f("%d번 게시물입니다.", id),article);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
