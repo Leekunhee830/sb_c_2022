022b83198b88e14f8a2735cb1ce2b9671d980839
Leekunhee830 <gorrka159@naver.com>
2022-03-30 22:48:13 +0900

    JSP적용

--- src/main/java/com/lkh/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrHomeController.java
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UsrHomeController {
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String doJoin() {
-		return "안녕하세요.";
+	public String showMain() {
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 	
 }
