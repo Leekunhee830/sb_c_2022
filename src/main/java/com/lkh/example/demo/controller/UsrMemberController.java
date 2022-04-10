0b10cbbaad11fbda493c77c3b8dec517d887d0f3
Leekunhee830 <gorrka159@naver.com>
2022-04-10 22:31:52 +0900

    로그인 처리에 Rq 적용

--- src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/lkh/example/demo/controller/UsrMemberController.java
@@ -2,6 +2,7 @@ package com.lkh.example.demo.controller;
 
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import com.lkh.example.demo.service.MemberService;
 import com.lkh.example.demo.util.Ut;
 import com.lkh.example.demo.vo.Member;
 import com.lkh.example.demo.vo.ResultData;
+import com.lkh.example.demo.vo.Rq;
 
 
 
@@ -81,14 +83,10 @@ public class UsrMemberController {
 	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(HttpSession httpSession, String loginId,String loginPw) {
-		boolean isLogined=false;
+	public String doLogin(HttpServletRequest req, String loginId,String loginPw) {
+		Rq rq=(Rq)req.getAttribute("rq");
 		
-		if(httpSession.getAttribute("loginedMemberId")!=null) {
-			isLogined=true;
-		}
-		
-		if(isLogined) {
+		if(rq.isLogined()) {
 			return Ut.jsHistoryBack("이미 로그인되었습니다.");
 		}
 		
@@ -109,7 +107,7 @@ public class UsrMemberController {
 			return Ut.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
 		
-		httpSession.setAttribute("loginedMemberId", member.getId());
+		rq.login(member);
 		
 		return Ut.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()),"/");
 		
