eff474bc647f7550570c56340ba5f50381d247cb
Leekunhee830 <gorrka159@naver.com>
2022-04-19 20:11:40 +0900

    게시물리스트에서 존재하지 않는 게시판 체크

--- src/main/java/com/lkh/example/demo/service/BoardService.java
+++ src/main/java/com/lkh/example/demo/service/BoardService.java
@@ -1,17 +1,10 @@
 package com.lkh.example.demo.service;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.lkh.example.demo.repository.ArticleRepository;
 import com.lkh.example.demo.repository.BoardRepository;
-import com.lkh.example.demo.util.Ut;
-import com.lkh.example.demo.vo.Article;
 import com.lkh.example.demo.vo.Board;
-import com.lkh.example.demo.vo.ResultData;
+
 
 @Service
 public class BoardService {
